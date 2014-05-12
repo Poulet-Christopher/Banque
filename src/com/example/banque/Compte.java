0bb5f753e79aadf8376d9c6b54ebd001dfa051fb
Poulet-Christopher <poulet.christopher@gmail.com>
2014-05-12 22:51:06 +0200

    debugage vues + class compte

---
+++ src/com/example/banque/Compte.java
@@ -0,0 +1,47 @@
+package com.example.banque;
+
+public class Compte {
+	
+	private String name;
+	private int ID=0;
+	private double solde;
+	
+	public void Compte(String name, double solde){
+		ID = ID+1;
+		this.name = name;
+		this.solde = solde;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public int getId(){
+		return this.ID;
+	}
+	
+	private void setId(int ID){
+		this.ID = ID;
+	}
+	
+	public void setSolde(double solde){
+		this.solde = solde;
+	}
+	
+	public double getSolde(){
+		return this.solde;
+	}
+	
+	public void crediter(double montant){
+		this.solde += montant;  
+	}
+	
+	public void debiter(double montant){
+		this.solde -= montant;
+	}
+
+}
